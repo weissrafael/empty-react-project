95220483a305227e98cd028d79e96e412441be06
weissrafael <dataweiss@gmail.com>
2023-07-24 12:39:08 -0300

    message details

--- src/Components/Message/Message.tsx
+++ src/Components/Message/Message.tsx
@@ -4,7 +4,6 @@ import { mockedLoggedUser } from '../../API/mockedLoggedUser';
 import { AWSUserAvatarUrl } from '../../Constants/AWS';
 import { ContactResource } from '../../Models/ContactResource';
 import { MessageResource } from '../../Models/MessageResource';
-import { ContactAvatar } from '../ContactCard/styles';
 
 import {
   ChatAvatar,
@@ -23,29 +22,26 @@ interface Props {
 export default function Message({ message, members }: Props) {
   const membersIds = members?.map((member) => member.id);
   const isFromOtherUser = message?.userId !== mockedLoggedUser.id;
-  const isGroup = membersIds?.length && membersIds?.length > 2;
+  const isGroup = membersIds && membersIds?.length > 2;
   const messageOwner = members?.find((member) => member.id === message?.userId);
   const capitalName = messageOwner
     ? messageOwner.name.charAt(0).toUpperCase() + messageOwner.name.slice(1)
     : '';
   const avatarUrl = AWSUserAvatarUrl + 'user' + messageOwner?.id + '.png';
-  if (isGroup && isFromOtherUser) {
-    return (
-      <MessageBody isFromOtherUser={isFromOtherUser}>
-        <MessageContent isGroup={isGroup} isFromOtherUser={isFromOtherUser}>
-          <ChatAvatar src={avatarUrl} />
-          <InfoContainer>
-            <MemberName>{capitalName}</MemberName>
-            <MessageText>{message?.text}</MessageText>
-          </InfoContainer>
-        </MessageContent>
-      </MessageBody>
-    );
-  }
   return (
     <MessageBody isFromOtherUser={isFromOtherUser}>
-      <MessageContent isFromOtherUser={isFromOtherUser}>
-        {message?.text}
+      <MessageContent isGroup={isGroup} isFromOtherUser={isFromOtherUser}>
+        {isGroup && isFromOtherUser ? (
+          <>
+            <ChatAvatar src={avatarUrl} />
+            <InfoContainer>
+              <MemberName>{capitalName}</MemberName>
+              <MessageText>{message?.text}</MessageText>
+            </InfoContainer>
+          </>
+        ) : (
+          message?.text
+        )}
       </MessageContent>
     </MessageBody>
   );
