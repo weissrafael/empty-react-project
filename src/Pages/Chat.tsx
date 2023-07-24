40eb59a8e996fac1d68a2461c118486e97df9a6a
weissrafael <dataweiss@gmail.com>
2023-07-24 16:08:26 -0300

    empty states

--- src/Pages/Chat.tsx
+++ src/Pages/Chat.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import ChatWindow from '../Components/ChatWindow/ChatWindow';
+import EmptyState from '../Components/EmptyState/EmptyState';
+import ErrorState from '../Components/ErrorState/ErrorState';
 import FullScreenLoader from '../Components/FullscreenLoader/FullScreenLoader';
 import useConversations from '../Hooks/useConversations';
 import useMessages from '../Hooks/useMessages';
@@ -9,28 +11,35 @@ import { useChatStore } from '../Stores/chat';
 
 function Chat() {
   const { id } = useParams();
-  const { data: dataFromApi } = useMessages.useGetMessage(id || '');
-  const { data, isLoading } = useConversations.useGetSingleConversation(
-    id || ''
-  );
+  const {
+    data: messageData,
+    isError: messageError,
+    isLoading: messageLoading,
+  } = useMessages.useGetMessage(id || '');
+  const { data, isLoading, isError } =
+    useConversations.useGetSingleConversation(id || '');
   const { setChatIsLoading, setSelectedConversation } = useChatStore(
     (state) => state
   );
 
   useEffect(() => {
     setChatIsLoading(false);
-  }, [dataFromApi, setChatIsLoading]);
+  }, [messageData, setChatIsLoading]);
 
   useEffect(() => {
     if (data.id) setSelectedConversation(data);
   }, [data, setSelectedConversation]);
 
-  return (
-    <>
-      <FullScreenLoader isLoading={isLoading} />
-      <ChatWindow members={data.members} messages={dataFromApi} />
-    </>
-  );
+  if (isError || messageError) return <ErrorState />;
+  else if (messageLoading || isLoading) return <FullScreenLoader isLoading />;
+  else if (messageData.length === 0)
+    return (
+      <EmptyState
+        title="No messages here!"
+        subtitle="Type on the box bellow to start a conversation"
+      />
+    );
+  else return <ChatWindow members={data.members} messages={messageData} />;
 }
 
 export default React.memo(Chat);
